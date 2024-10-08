9fb0b6ba12bc4a89db20a04a6992f081a3c90a29
bavlywagih696@gmail.com <93160602+bavlywagih@users.noreply.github.com>
2024-10-08 19:13:21 +0300

    Delete v-25 directory

--- v-25/fetch_gate_count.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'connect.php'; // الاتصال بقاعدة البيانات
-
-$query = "SELECT COUNT(*) AS count FROM gates";
-$stmt = $pdo->query($query);
-$data = $stmt->fetch(PDO::FETCH_ASSOC);
-
-echo json_encode($data);
-?>
